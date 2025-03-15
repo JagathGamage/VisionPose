44809c6e3b9a78bfb4c5836255346d7768e16aab
jagath <jagathdammika17595@gmail.com>
2025-03-15 11:43:26 +0530

    resources

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import "./App.css";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {Login,Signup} from "./Routes.js";
+import RequirementSelector from './components/RequirementSelector.js';
+import VideoTrimmer from './components/VideoTrimmer.js';
+import GraphDashboard from './components/Graphs.js';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path='/Login' element={<Login />} />
         <Route path='/Signup' element={<Signup />} />
+        <Route path='/requirementSelector' element={<RequirementSelector />} />
+        <Route path='/videoTrimmer' element={<VideoTrimmer />} />
+        <Route path='/graphs' element={<GraphDashboard />} />
       </Routes>
     </BrowserRouter>
   );
